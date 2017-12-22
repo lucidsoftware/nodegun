cbfe72efa9a9ecacedc6a7990136fdb3c6ef146c
Paul Draper <paulddraper@gmail.com>
2017-12-22 01:39:56 -0700

    Strip shebang

--- commandfactory.ts
+++ commandfactory.ts
@@ -20,6 +20,50 @@ class Resolver {
     }
 }
 
+/**
+ * Copied from internal/module
+ */
+function stripBOM(content: string) {
+  if (content.charCodeAt(0) === 0xFEFF) {
+    content = content.slice(1);
+  }
+  return content;
+}
+
+/**
+ * Copied from internal/module
+ */
+function stripShebang(content: string) {
+  // Remove shebang
+  var contLen = content.length;
+  if (contLen >= 2) {
+    if (content.charCodeAt(0) === 35/*#*/ &&
+        content.charCodeAt(1) === 33/*!*/) {
+      if (contLen === 2) {
+        // Exact match
+        content = '';
+      } else {
+        // Find end of shebang line and slice it off
+        var i = 2;
+        for (; i < contLen; ++i) {
+          var code = content.charCodeAt(i);
+          if (code === 10/*\n*/ || code === 13/*\r*/)
+            break;
+        }
+        if (i === contLen)
+          content = '';
+        else {
+          // Note that this actually includes the newline character(s) in the
+          // new output. This duplicates the behavior of the regular expression
+          // that was previously used to replace the shebang line
+          content = content.slice(i);
+        }
+      }
+    }
+  }
+  return content;
+}
+
 export class CommandFactory {
     private readonly cache = new Map<string, Command>();
     private readonly resolver = new Resolver();
@@ -32,10 +76,8 @@ export class CommandFactory {
             require.extensions['.js'] = (module: NodeModule, filename: string) => {
                 module.id = '.';
                 let content = fs.readFileSync(resolved, 'utf-8');
-                // remove BOM, copied from internal/module stripBOM
-                if (content.charCodeAt(0) === 0xFEFF) {
-                   content = content.slice(1);
-                }
+                content = stripBOM(content);
+                content = stripShebang(content);
                 module._compile(`require.main = process.mainModule = module; module.exports = () => {${content}\n};`, filename);
                 require.extensions['.js'] = oldJs;
             };
